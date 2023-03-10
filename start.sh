dcb8f5971b8797a541830092989d0593839997be
CrezyDeveloper143 <117562112+CrezyDeveloper143@users.noreply.github.com>
2023-03-10 10:07:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Greymattersbot/Link-Search-Bot-Pro /MdiskWalaBot
-cd /MdiskWalaBot
+git clone https://github.com/CrezyDeveloper143/Maxx_Link_search/Maxx_Link_search
+cd /Maxx_Link_search
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
